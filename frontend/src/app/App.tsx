f448a89e8e6a0ed87e674add624b7c8d8d7ad977
Ivan Besedin <ivanbesedin.05@gmail.com>
2024-06-14 00:34:22 +0300

    added redirect to 1st page

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,4 +1,4 @@
-import { Routes, useLocation, Route } from 'react-router-dom'
+import { Routes, useLocation, Route, Navigate } from 'react-router-dom'
 import { AnimatePresence } from "framer-motion"
 import './App.scss'
 import { FirstStep, PageOutlet, SecondStep, ThirdStep } from '../Components'
@@ -10,7 +10,7 @@ function App() {
         <AnimatePresence initial={true} mode="wait">
           <Routes location={location} key={location.pathname}>
             <Route path="/" element={<PageOutlet />}>
-                <Route index element={<h1>Начало</h1>}/>
+                <Route index element={<Navigate to='/1'/>}/>
                 <Route path='1' element={<FirstStep/>}/>
                 <Route path='2' element={<SecondStep/>}/>
                 <Route path='3' element={<ThirdStep/>}/>
