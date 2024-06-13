73093da6640eda612230c8be25d256de1883fd42
Ivan Besedin <ivanbesedin.05@gmail.com>
2024-06-13 16:59:27 +0300

    added 3 first inputs

--- frontend/src/Components/PageOutlet/PageOutlet.tsx
+++ frontend/src/Components/PageOutlet/PageOutlet.tsx
@@ -1,14 +1,37 @@
 import { Outlet } from "react-router"
 import Header from "../Header/Header"
 import styles from './PageOutlet.module.scss'
+import { FormProvider, useForm } from "react-hook-form"
 
 const PageOutlet = () => {
+	interface FormValues {
+		apartment_type: 'secondary' | 'new',
+		metro: string,
+		minutes_to_metro: number
+		rooms_number: number
+		area: number
+		kitchen_area: number
+		floor: number
+		floors_number: number
+		renovation: 'cosmetic' | 'no' | 'european-style' | 'designer'
+	}
+
+	const methods = useForm<FormValues>()
+	const {
+		handleSubmit,
+	} = methods
+
+	const onSubmit = (data: FormValues) => {
+		console.log(data)
+	}
 	return (
 	  <div>
 		<Header />
-		<form action="" >
-			<Outlet />
-		</form>
+			<FormProvider {...methods}>
+				<form action="" onSubmit={handleSubmit(onSubmit)} className={styles.form}>
+					<Outlet />
+				</form>
+			</FormProvider>
 	  </div>
 	)
   }
--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,6 +1,9 @@
 import { Routes, useLocation, Route } from 'react-router-dom'
 import { AnimatePresence } from "framer-motion"
 import PageOutlet from '../Components/PageOutlet/PageOutlet'
+import Input from '../Components/Input/Input'
+import SelectInput from '../Components/SelectInput/SelectInput'
+import './App.scss'
 
 function App() {
   const location = useLocation()
@@ -9,7 +12,33 @@ function App() {
           <Routes location={location} key={location.pathname}>
             <Route path="/" element={<PageOutlet />}>
                 <Route index element={<h1>Начало</h1>}/>
-                <Route path='1' element={<h1>Шаг 1</h1>}/>
+                <Route path='1' element={
+                  <>
+                    <SelectInput
+                      label='Тип жилья'
+                      placeholder='Выберите тип жилья'
+                      name='apartment_type'
+                      items={[
+                        {value: 'new', label: 'Новостройка'},
+                        {value: 'secondary', label: 'Вторичка'},
+                      ]}
+                      onClick={() => {}}
+                      height='56px'
+                    />
+                    <Input
+                      label='Время в пути до метро' 
+                      placeholder='Введите в минутах'
+                      name='minutes_to_metro'
+                      isRequired={true}
+                      />
+                    <Input
+                      label='Название станции метро' 
+                      placeholder='Введите название ближайшей'
+                      name='metro'
+                      isRequired={true}
+                    />
+                  </>
+                }/>
                 <Route path='2' element={<h1>Шаг 2</h1>}/>
                 <Route path='3' element={<h1>Шаг 3</h1>}/>
             </Route>
